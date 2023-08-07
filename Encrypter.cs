004ec67a0bacc2884cda2c114e6637386a8ebdc5
MP-Hartvig <mip@hartvigconsult.dk>
2023-08-07 12:52:05 +0200

    Add project files.

---
+++ Encrypter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomTest
+{
+    internal class Encrypter
+    {
+        public static string Encrypt(string txt)
+        {
+            string result = "";
+
+            var txtArray = txt.ToCharArray();
+
+            foreach (var item in txtArray)
+            {
+                var temp = item;
+                char[] cc = new char[1] { ++temp };
+
+                byte[] bytes = Encoding.UTF8.GetBytes(cc);
+
+                result += Encoding.UTF8.GetString(bytes);
+            }
+
+            return result;
+        }
+
+        public static string Decrypt(string txt)
+        {
+            string result = "";
+
+            var txtArray = txt.ToCharArray();
+
+            foreach (var item in txtArray)
+            {
+                var temp = item;
+                char[] cc = new char[1] { --temp };
+
+                byte[] bytes = Encoding.UTF8.GetBytes(cc);
+
+                result += Encoding.UTF8.GetString(bytes);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using RandomTest;
+using System.Security.Cryptography;
+
+var rng = RandomNumberGenerator.Create();
+
+byte[] rBytes = new byte[4];
+
+rng.GetBytes(rBytes);
+
+int randomNumber = BitConverter.ToInt32(rBytes, 0);
+
+//RunBenchmark(RandomGenerator, "RandomGenerator");
+
+//RunBenchmark(TrueRandomGenerator, "TrueRandomGenerator");
+
+string encrypt = Encrypter.Encrypt("Hello");
+
+Console.WriteLine(encrypt);
+
+string decrypt = Encrypter.Decrypt(encrypt);
+
+Console.WriteLine(decrypt);
+
+
+// ------------------------------------------------
+
+void RandomGenerator()
+{
+    for (int i = 0; i < 1000000; i++)
+    {
+        Random rand = new Random();
+
+        rand.Next(999);
+    }
+}
+
+void TrueRandomGenerator()
+{
+    for (int i = 0; i < 1000000; i++)
+    {
+        var rand = RandomNumberGenerator.GetInt32(1000);
+    }
+}
+
+// ------------------------------
+
+void RunBenchmark(Action benchmarkMethod, string methodName)
+{
+    long startTime = DateTime.Now.Ticks;
+    benchmarkMethod();
+    long endTime = DateTime.Now.Ticks;
+    long elapsedTime = endTime - startTime;
+    Console.WriteLine($"{methodName} tid (ticks): {elapsedTime}");
+}
+
+// ------------------------------
+
